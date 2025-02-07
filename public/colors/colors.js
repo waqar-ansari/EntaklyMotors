fae76dc4a4ebb0355ee0a36b2aa8a0a62cffdd0a
Waqar Ansari <waqar.78692@yahoo.com>
2025-02-07 18:55:21 +0400

    changes

---
+++ public/colors/colors.js
@@ -0,0 +1,7 @@
+
+export const colors = {
+    themeMain: '#292268',
+    themeSecondary: '#ff0000',
+    black: '#000',
+    white: '#fff',
+  };
---
+++ public/fonts/fonts.js
@@ -0,0 +1,8 @@
+import '../fonts/fonts.css'
+
+export const fonts = {
+    helvetica400: 'helvetica400',
+    helvetica700: 'helvetica700',
+    helvetica900: 'helvetica900',
+    times400: 'times400',
+  };
